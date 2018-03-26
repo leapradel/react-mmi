64cfc6ae7a0069ce824421976f7d99603ae479aa
Lea <lea.pradel@etu.univ-grenoble-alpes.fr>
2018-03-26 13:01:44 +0000

    ajout d'un bouton

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,27 +15,55 @@ import ReactDOM from 'react-dom';
 // On peut aussi importer du CSS de la meme facon.
 import '../../components/style.css';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      machines: [ 
+      { name: "Machine 1",
+        isActive: true
+      },
+      { name: "Machine 2",
+        isActive: false
+      },
+     { name: "Machine 3",
+        isActive: false
+      },
+     { name: "Machine 4",
+        isActive: false
+      }
+       ]
+    };
+      console.log(this.state);
+      console.log(Object.keys(this.state.machines));
+  }
 
-
-export default function App() {
-  return (
-    <div>
-    <Header/>
-    <Machine number="1" isActive />
-    <Toggle/>
-    <Machine number="2"/>
-    <Toggle/>
-    <Machine number="3"/>
-    <Toggle/>
-    <Machine number="111111" isActive/>
-    <Toggle/>
+  
+  render () {
+    
+    return (
+      
+      <div>
+         <Header/>
+         
+      {this.state.machines.map(machine => 
+        <Machine name={machine.name}
+                  key={machine.key}
+                isActive={machine.isActive}
+        />
+        
+        )
+      }
     <Footer/>
     </div>
+      );
+}
+
+}
     
+  export default App;
     
     // Un return doit retourner un seul élément du DOM
     // Si on veut afficher plusieurs éléments adjacents,
     // On devra donc les encapsuler dans une DIV parente.
    
-  );
-}
